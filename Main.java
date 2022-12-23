a4aa96839e344d35817c230ca1ef56d40828df14
omerkorkmz <97345262+omerkorkmz@users.noreply.github.com>
2022-12-23 21:51:01 +0300

    Ödev-1 (ATM ödevi)

--- Main.java
+++ Main.java
@@ -1,40 +1,74 @@
-package random;
-import java.util.Random;
 import java.util.Scanner;
 
-//0 ile 500 aralığında olan sayılardan 50 ile 100 arasında bir sayı bulunana kadar rastgele sayılar seçilir.
-//50 ile 100 arasındaki sayının kaçıncı seçimde bulunduğunu bulan program.
+
 public class Main {
-    public static void main(String[] args) {
-        int sayac = 0, i = 0;
-        int uretilenSayi = 0;
 
-        Random r = new Random();
+    public static void main(String[]args) {
 
-        while (i<1){
-            uretilenSayi = r.nextInt(500);
+        Scanner omer = new Scanner(System.in);
 
-            if(50 <uretilenSayi & uretilenSayi < 101){
+        int bakiye = 50000;
 
-                break;
+
+        System.out.println("BankATM'ye hosgeldiniz");
+
+
+
+
+
+        System.out.println("Bakiye sorgulamak icin 1 nolu tusu tercih ediniz");
+        System.out.println("Para cekeme islemleri  icin 2 nolu tusu tercih ediniz");
+        System.out.println("Para yatırma islemleri icin 3 nolu tusu tercih ediniz");
+        System.out.println("Cıkıs yapmak icin 4 nolu tusu tercih ediniz");
+
+
+        while (true) {
+            System.out.println("Lütfen işleminizi giriniz");
+            int islemler = omer.nextInt();
+            switch (islemler) {
+
+                case 1:
+                    System.out.println("Guncel bakiyeniz:" +bakiye+"TL'dir. ");
+
+                    break;
+                case 2:
+                    System.out.println("Cekmek istediginiz miktari giriniz: ");
+                    int paraCek = omer.nextInt();
+
+                    if (paraCek < bakiye)
+                    {
+                        bakiye -= paraCek;
+                        System.out.println("İsleminizi gerceklestiriyoruz: "+bakiye+" kalan bakiyeniz ");
+                    }
+                    else
+                    {
+                        System.out.println("Bakiyenizden yuksek bir miktar giridiniz");
+                    }
+
+                    break;
+
+                case 3:
+                    System.out.println("Yatirmak istediginiz miktari girin: ");
+                    int paraYatir = omer.nextInt();
+                    bakiye += paraYatir;
+                    System.out.println("Guncel bakiyeniz: " +bakiye+ "'dir ");
+                    break;
+
+
+                case 4:
+                    System.out.println("BankATM yi tercih ettiginiz icin tesekkur ederiz");
+
+
             }
-            sayac = sayac+1;
         }
-        System.out.println(sayac+": Seferde  "+uretilenSayi);
+
     }
+
+
+
+
+
+
+
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
